abdfcc69fd53abfbbeb7e2880340522da3b5911c
north151 <70125880+north151@users.noreply.github.com>
2022-04-05 09:10:31 +0800

    Add files via upload

---
+++ 哪吒探针默认主题/哪吒探针默认主题.js
@@ -0,0 +1,13 @@
+< script >
+
+    window.onload = function() {
+        var avatar = document.querySelector(".item img")
+
+        var footer = document.querySelector("div.is-size-7")
+        footer.innerHTML = "north151"
+        footer.style.visibility = "visible"
+
+        avatar.src = "LOGO路径"
+        avatar.style.visibility = "visible"
+    } 
+</script>
